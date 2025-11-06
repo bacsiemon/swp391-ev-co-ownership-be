06dd3519608a762d48c2214ad57c00ae35da0d15
bacsiemon <johnsieunhan816@gmail.com>
2025-11-06 16:25:23 +0700

    [minor] fix enum default value

--- EvCoOwnership.Repositories/Enums/EContractTemplateType.cs
+++ EvCoOwnership.Repositories/Enums/EContractTemplateType.cs
@@ -8,41 +8,41 @@ namespace EvCoOwnership.Repositories.Enums
         /// <summary>
         /// Co-ownership agreement contract
         /// </summary>
-        CoOwnershipAgreement = 1,
+        CoOwnershipAgreement = 0,
 
         /// <summary>
         /// Vehicle usage agreement
         /// </summary>
-        VehicleUsageAgreement = 2,
+        VehicleUsageAgreement = 1,
 
         /// <summary>
         /// Cost sharing agreement
         /// </summary>
-        CostSharingAgreement = 3,
+        CostSharingAgreement = 2,
 
         /// <summary>
         /// Maintenance responsibility agreement
         /// </summary>
-        MaintenanceAgreement = 4,
+        MaintenanceAgreement = 3,
 
         /// <summary>
         /// Insurance agreement
         /// </summary>
-        InsuranceAgreement = 5,
+        InsuranceAgreement = 4,
 
         /// <summary>
         /// Ownership transfer agreement
         /// </summary>
-        OwnershipTransferAgreement = 6,
+        OwnershipTransferAgreement = 5,
 
         /// <summary>
         /// Dispute resolution agreement
         /// </summary>
-        DisputeResolutionAgreement = 7,
+        DisputeResolutionAgreement = 6,
 
         /// <summary>
         /// Exit/Termination agreement
         /// </summary>
-        ExitAgreement = 8
+        ExitAgreement = 7
     }
 }
